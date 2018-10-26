088b366f7b25f6387ed14b4e48c0839d7ba68409
huyenbt <huyenbt2208@gmail.com>
2018-10-26 17:09:06 +0700

    page courses

---
+++ course.php
@@ -0,0 +1,57 @@
+<?php $page = 'homepage'; include "./config/include.php"; ?>
+
+<!-- *** stylesheet *** -->
+<?php include "./templates/common_css.php"; ?>
+<link href="./css/<?php echo $page; ?>.css" rel="stylesheet" type="text/css" media="all">
+
+<!-- *** javascript *** -->
+<?php include "./templates/common_js.php"; ?>
+<script src="./js/homepage.js"></script>
+</head>
+
+<body id="<?php echo $page; ?>">
+<?php include "./templates/header.php"; ?>
+<div id="contents">
+<div class="wrap">
+	<div class="con_courses">
+		<div class="box_courses">
+			<div class="box_course">
+				<div class="box_top">
+					<p class="photo"><img src="./images/course/img_course.png" alt="course 01" /></p>
+					<h3 class="sst_title">
+						<img src="./images/course/img_couse_user1.png" alt="course 01" />
+						<span class="name">Phạm Thành Long</span>
+					</h3>
+				</div>
+				<div class="box_info">
+					<h4 class="sst_title">Khoá học lãnh đạo và quản lý</h4>
+					<div class="box_price">
+						<span class="price">599.000đ</span>
+						<span class="price_sale">  2.000.000đ    </span>
+						<span class="price_percent"> -70%</span>
+					</div>
+					<div class="box_desc">
+						<ul class="add-to-links">
+							 <li>
+								 <a class="#" href="#" title="Add to Wishlist">
+									 <i class="fa fa-heart">10</i>
+								 </a>
+							 </li>
+							 <li>
+								 <a class="#" href="#" title="Add to Compare">
+									 <i class="fa fa-random"></i>
+								 </a>
+							 </li>
+							
+						 </ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+</div><!-- /#contents -->
+<?php include "./templates/footer.php"; ?>
+</body>
+</html>
--- templates/common_css.php
+++ templates/common_css.php
@@ -1,3 +1,4 @@
 <link href="<?php echo location_file; ?>css/default.css" rel="stylesheet" type="text/css" media="all">
 <link href="<?php echo location_file; ?>css/print.css" rel="stylesheet" type="text/css" media="print">
 <link href="<?php echo location_file; ?>css/common.css" rel="stylesheet" type="text/css" media="all">
+<link rel="stylesheet" type="text/css" href="font/font-awesome/css/font-awesome.min.css" />     
