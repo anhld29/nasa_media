e6aeeff033cb93a76416cb6ffaaf6e18120f43a5
Le Duc Anh <anhld29@gmail.com>
2018-10-27 00:29:37 +0700

    a

--- templates/header.php
+++ templates/header.php
@@ -1,4 +1,8 @@
 <div id="page">
 <div id="header">
-	
+	<div class="con-header">
+		<div class="wrap">
+			<div class="box-logo"><a href="#" class="over"><img src="<?php echo location_file;?>images/logo.png" alt="Nasa Media"></a></div>
+		</div>
+	</div>
 </div><!-- /#header -->
