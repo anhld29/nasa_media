52ab409dc87ae52595090ad81aabae78c0e94821
huyenbt <huyenbt2208@gmail.com>
2018-10-31 22:33:48 +0700

    bug

--- index.php
+++ index.php
@@ -183,7 +183,7 @@
             <p class="photo"><img src="./images/course/img_course.png" alt="course 01" /></p>
             <h4 class="box_name">
               <img src="./images/course/img_couse_user1.png" alt="course 01" />
-              <span class="name">Phạm Thành Long</span>
+              <span class="name">Tuấn Hạ</span>
             </h4>
           </div>
           <div class="box_info">
@@ -222,47 +222,7 @@
             <p class="photo"><img src="./images/course/img_course.png" alt="course 01" /></p>
             <h4 class="box_name">
               <img src="./images/course/img_couse_user1.png" alt="course 01" />
-              <span class="name">Phạm Thành Long</span>
-            </h4>
-          </div>
-          <div class="box_info">
-            <h4 class="sst_title">Khoá học lãnh đạo và quản lý</h4>
-            <h6 class="name">Phạm Thành Long</h6>
-            <div class="box_price">
-              <span class="price">599.000đ</span>
-              <span class="price_sale">  2.000.000đ    </span>
-              <span class="price_percent"> -70%</span>
-            </div>
-            <div class="box_desc">
-              <ul class="add-to-links">
-                 <li>
-                   <a class="#" href="#" title="Add to Wishlist">
-                     <i class="fa fa-heart"></i> <span>342</span>
-                   </a>
-                 </li>
-                 <li>
-                   <a class="#" href="#" title="Add to Compare">
-                     <i class="fa fa-comments"></i> <span>10</span>
-                   </a>
-                 </li>
-                 <li>
-                  <div class="star-rating">
-                  <span style="width:80%"><strong class="rating">4.00</strong> out of 5</span>
-                  </div>
-                 </li>
-                
-               </ul>
-            </div>
-          </div>
-        </div>
-      </li>
-      <li class="box_course">
-        <div class="box_course_wrap">
-          <div class="box_top">
-            <p class="photo"><img src="./images/course/img_course.png" alt="course 01" /></p>
-            <h4 class="box_name">
-              <img src="./images/course/img_couse_user1.png" alt="course 01" />
-              <span class="name">Phạm Thành Long</span>
+              <span class="name">Tuấn Hạ</span>
             </h4>
           </div>
           <div class="box_info">
@@ -302,7 +262,47 @@
             <p class="photo"><img src="./images/course/img_course.png" alt="course 01" /></p>
             <h4 class="box_name">
               <img src="./images/course/img_couse_user1.png" alt="course 01" />
-              <span class="name">Phạm Thành Long</span>
+              <span class="name">Tuấn Hạ</span>
+            </h4>
+          </div>
+          <div class="box_info">
+            <h4 class="sst_title">Khoá học lãnh đạo và quản lý</h4>
+            <h6 class="name">Phạm Thành Long</h6>
+            <div class="box_price">
+              <span class="price">599.000đ</span>
+              <span class="price_sale">  2.000.000đ    </span>
+              <span class="price_percent"> -70%</span>
+            </div>
+            <div class="box_desc">
+              <ul class="add-to-links">
+                 <li>
+                   <a class="#" href="#" title="Add to Wishlist">
+                     <i class="fa fa-heart"></i> <span>342</span>
+                   </a>
+                 </li>
+                 <li>
+                   <a class="#" href="#" title="Add to Compare">
+                     <i class="fa fa-comments"></i> <span>10</span>
+                   </a>
+                 </li>
+                 <li>
+                  <div class="star-rating">
+                  <span style="width:80%"><strong class="rating">4.00</strong> out of 5</span>
+                  </div>
+                 </li>
+                
+               </ul>
+            </div>
+          </div>
+        </div>
+      </li>
+      <li class="box_course">
+        <div class="box_course_wrap">
+          <div class="box_top">
+            <p class="photo"><img src="./images/course/img_course.png" alt="course 01" /></p>
+            <h4 class="box_name">
+              <img src="./images/course/img_couse_user1.png" alt="course 01" />
+              <span class="name">Tuấn Hạ</span>
             </h4>
           </div>
           <div class="box_info">
@@ -415,7 +415,7 @@
             <p class="photo"><img src="./images/course/img_course.png" alt="course 01" /></p>
             <h4 class="box_name">
               <img src="./images/course/img_couse_user1.png" alt="course 01" />
-              <span class="name">Phạm Thành Long</span>
+              <span class="name">Tuấn Hạ</span>
             </h4>
           </div>
           <div class="box_info">
@@ -454,47 +454,7 @@
             <p class="photo"><img src="./images/course/img_course.png" alt="course 01" /></p>
             <h4 class="box_name">
               <img src="./images/course/img_couse_user1.png" alt="course 01" />
-              <span class="name">Phạm Thành Long</span>
-            </h4>
-          </div>
-          <div class="box_info">
-            <h4 class="sst_title">Khoá học lãnh đạo và quản lý</h4>
-            <h6 class="name">Phạm Thành Long</h6>
-            <div class="box_price">
-              <span class="price">599.000đ</span>
-              <span class="price_sale">  2.000.000đ    </span>
-              <span class="price_percent"> -70%</span>
-            </div>
-            <div class="box_desc">
-              <ul class="add-to-links">
-                 <li>
-                   <a class="#" href="#" title="Add to Wishlist">
-                     <i class="fa fa-heart"></i> <span>342</span>
-                   </a>
-                 </li>
-                 <li>
-                   <a class="#" href="#" title="Add to Compare">
-                     <i class="fa fa-comments"></i> <span>10</span>
-                   </a>
-                 </li>
-                 <li>
-                  <div class="star-rating">
-                  <span style="width:80%"><strong class="rating">4.00</strong> out of 5</span>
-                  </div>
-                 </li>
-                
-               </ul>
-            </div>
-          </div>
-        </div>
-      </li>
-      <li class="box_course">
-        <div class="box_course_wrap">
-          <div class="box_top">
-            <p class="photo"><img src="./images/course/img_course.png" alt="course 01" /></p>
-            <h4 class="box_name">
-              <img src="./images/course/img_couse_user1.png" alt="course 01" />
-              <span class="name">Phạm Thành Long</span>
+              <span class="name">Tuấn Hạ</span>
             </h4>
           </div>
           <div class="box_info">
@@ -534,7 +494,7 @@
             <p class="photo"><img src="./images/course/img_course.png" alt="course 01" /></p>
             <h4 class="box_name">
               <img src="./images/course/img_couse_user1.png" alt="course 01" />
-              <span class="name">Phạm Thành Long</span>
+              <span class="name">Tuấn Hạ</span>
             </h4>
           </div>
           <div class="box_info">
@@ -574,7 +534,7 @@
             <p class="photo"><img src="./images/course/img_course.png" alt="course 01" /></p>
             <h4 class="box_name">
               <img src="./images/course/img_couse_user1.png" alt="course 01" />
-              <span class="name">Phạm Thành Long</span>
+              <span class="name">Tuấn Hạ</span>
             </h4>
           </div>
           <div class="box_info">
@@ -614,7 +574,7 @@
             <p class="photo"><img src="./images/course/img_course.png" alt="course 01" /></p>
             <h4 class="box_name">
               <img src="./images/course/img_couse_user1.png" alt="course 01" />
-              <span class="name">Phạm Thành Long</span>
+              <span class="name">Tuấn Hạ</span>
             </h4>
           </div>
           <div class="box_info">
@@ -654,7 +614,7 @@
             <p class="photo"><img src="./images/course/img_course.png" alt="course 01" /></p>
             <h4 class="box_name">
               <img src="./images/course/img_couse_user1.png" alt="course 01" />
-              <span class="name">Phạm Thành Long</span>
+              <span class="name">Tuấn Hạ</span>
             </h4>
           </div>
           <div class="box_info">
@@ -694,7 +654,47 @@
             <p class="photo"><img src="./images/course/img_course.png" alt="course 01" /></p>
             <h4 class="box_name">
               <img src="./images/course/img_couse_user1.png" alt="course 01" />
-              <span class="name">Phạm Thành Long</span>
+              <span class="name">Tuấn Hạ</span>
+            </h4>
+          </div>
+          <div class="box_info">
+            <h4 class="sst_title">Khoá học lãnh đạo và quản lý</h4>
+            <h6 class="name">Phạm Thành Long</h6>
+            <div class="box_price">
+              <span class="price">599.000đ</span>
+              <span class="price_sale">  2.000.000đ    </span>
+              <span class="price_percent"> -70%</span>
+            </div>
+            <div class="box_desc">
+              <ul class="add-to-links">
+                 <li>
+                   <a class="#" href="#" title="Add to Wishlist">
+                     <i class="fa fa-heart"></i> <span>342</span>
+                   </a>
+                 </li>
+                 <li>
+                   <a class="#" href="#" title="Add to Compare">
+                     <i class="fa fa-comments"></i> <span>10</span>
+                   </a>
+                 </li>
+                 <li>
+                  <div class="star-rating">
+                  <span style="width:80%"><strong class="rating">4.00</strong> out of 5</span>
+                  </div>
+                 </li>
+                
+               </ul>
+            </div>
+          </div>
+        </div>
+      </li>
+      <li class="box_course">
+        <div class="box_course_wrap">
+          <div class="box_top">
+            <p class="photo"><img src="./images/course/img_course.png" alt="course 01" /></p>
+            <h4 class="box_name">
+              <img src="./images/course/img_couse_user1.png" alt="course 01" />
+              <span class="name">Tuấn Hạ</span>
             </h4>
           </div>
           <div class="box_info">
@@ -764,7 +764,7 @@
             <p class="photo"><img src="./images/course/img_course.png" alt="course 01" /></p>
             <h4 class="box_name">
               <img src="./images/course/img_couse_user1.png" alt="course 01" />
-              <span class="name">Phạm Thành Long</span>
+              <span class="name">Tuấn Hạ</span>
             </h4>
           </div>
           <div class="box_info">
@@ -803,47 +803,7 @@
             <p class="photo"><img src="./images/course/img_course.png" alt="course 01" /></p>
             <h4 class="box_name">
               <img src="./images/course/img_couse_user1.png" alt="course 01" />
-              <span class="name">Phạm Thành Long</span>
-            </h4>
-          </div>
-          <div class="box_info">
-            <h4 class="sst_title">Khoá học lãnh đạo và quản lý</h4>
-            <h6 class="name">Phạm Thành Long</h6>
-            <div class="box_price">
-              <span class="price">599.000đ</span>
-              <span class="price_sale">  2.000.000đ    </span>
-              <span class="price_percent"> -70%</span>
-            </div>
-            <div class="box_desc">
-              <ul class="add-to-links">
-                 <li>
-                   <a class="#" href="#" title="Add to Wishlist">
-                     <i class="fa fa-heart"></i> <span>342</span>
-                   </a>
-                 </li>
-                 <li>
-                   <a class="#" href="#" title="Add to Compare">
-                     <i class="fa fa-comments"></i> <span>10</span>
-                   </a>
-                 </li>
-                 <li>
-                  <div class="star-rating">
-                  <span style="width:80%"><strong class="rating">4.00</strong> out of 5</span>
-                  </div>
-                 </li>
-                
-               </ul>
-            </div>
-          </div>
-        </div>
-      </li>
-      <li class="box_course">
-        <div class="box_course_wrap">
-          <div class="box_top">
-            <p class="photo"><img src="./images/course/img_course.png" alt="course 01" /></p>
-            <h4 class="box_name">
-              <img src="./images/course/img_couse_user1.png" alt="course 01" />
-              <span class="name">Phạm Thành Long</span>
+              <span class="name">Tuấn Hạ</span>
             </h4>
           </div>
           <div class="box_info">
@@ -883,7 +843,47 @@
             <p class="photo"><img src="./images/course/img_course.png" alt="course 01" /></p>
             <h4 class="box_name">
               <img src="./images/course/img_couse_user1.png" alt="course 01" />
-              <span class="name">Phạm Thành Long</span>
+              <span class="name">Tuấn Hạ</span>
+            </h4>
+          </div>
+          <div class="box_info">
+            <h4 class="sst_title">Khoá học lãnh đạo và quản lý</h4>
+            <h6 class="name">Phạm Thành Long</h6>
+            <div class="box_price">
+              <span class="price">599.000đ</span>
+              <span class="price_sale">  2.000.000đ    </span>
+              <span class="price_percent"> -70%</span>
+            </div>
+            <div class="box_desc">
+              <ul class="add-to-links">
+                 <li>
+                   <a class="#" href="#" title="Add to Wishlist">
+                     <i class="fa fa-heart"></i> <span>342</span>
+                   </a>
+                 </li>
+                 <li>
+                   <a class="#" href="#" title="Add to Compare">
+                     <i class="fa fa-comments"></i> <span>10</span>
+                   </a>
+                 </li>
+                 <li>
+                  <div class="star-rating">
+                  <span style="width:80%"><strong class="rating">4.00</strong> out of 5</span>
+                  </div>
+                 </li>
+                
+               </ul>
+            </div>
+          </div>
+        </div>
+      </li>
+      <li class="box_course">
+        <div class="box_course_wrap">
+          <div class="box_top">
+            <p class="photo"><img src="./images/course/img_course.png" alt="course 01" /></p>
+            <h4 class="box_name">
+              <img src="./images/course/img_couse_user1.png" alt="course 01" />
+              <span class="name">Tuấn Hạ</span>
             </h4>
           </div>
           <div class="box_info">
