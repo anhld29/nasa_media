8a2775ab3e63bcabfd83b44ef3bf7bbbb4c47950
huyenbt <huyenbt2208@gmail.com>
2018-11-01 22:55:41 +0700

    create cperation page

---
+++ cooperation.php
@@ -0,0 +1,124 @@
+<?php $page = 'about'; include "./config/include.php"; ?>
+
+<!-- *** stylesheet *** -->
+<?php include "./templates/common_css.php"; ?>
+
+<!-- *** javascript *** -->
+<?php include "./templates/common_js.php"; ?>
+</head>
+
+<body id="<?php echo $page; ?>">
+<?php include "./templates/header.php"; ?>
+<div id="contents">
+	<div class="page-title">
+		<div class="wrap">
+			<h2>NASA Media</h2>
+			<div class="sub-title">Nền tảng khóa học trực tuyến về kỹ năng cho lãnh đạo và quản lý trung cấp</div>
+		</div>
+	</div>
+	<div class="con_about">
+		<div class="box_about">
+			<div class="wrap">
+				<div class="box_item float_right">
+
+					<div class="box_info">
+						<p class="number">01</p>
+						<h3><span>Hợp tác giảng dạy</span></h3>
+						<ul>
+							<li>
+								<i class="fa fa-caret-right"></i>   Với sứ mệnh là cầu nối giữa giảng viên và học viên, hướng tới xây dựng một nền tảng khóa học kỹ năng trực tuyến rộng lớn – hiệu quả - uy tín nhất Việt Nam dành riêng cho cấp quản lý và lãnh đạo.  Tại Nasa media chúng tôi luôn tạo mọi điều kiện thuận lợi nhất để hình ảnh của các chuyên gia - giảng viên được tỏa sáng, đồng thời giúp truyền ngọn lửa đam mê này tới cho hàng triệu triệu học viên trên cả nước.
+
+							</li>
+						</ul>
+						
+					</div>
+					<p class="photo"><img src="./images/bg_about.png" alt="course 01" /></p>
+				</div>
+				<div class="box_item">
+
+					<div class="box_info">
+						<p class="number">02</p>
+						<h3><span>Mong muốn hợp tác</span></h3>
+						<p class="info">Chúng tôi mong muốn được hợp tác với: Các giảng viên/chuyên gia/chuyên viên/nhân sự cấp cao /cấp quản lý /doanh nhân có khả năng giảng dạy tốt ở một trong các chủ đề KIẾN THỨC/KỸ NĂNG DÀNH CHO LÃNH ĐẠO như:</p>
+						<ul>
+							<li><i class="fa fa-caret-right"></i>  Quản trị Nhân sự</li>
+							<li><i class="fa fa-caret-right"></i>  Quản trị chiến lược</li>
+							<li><i class="fa fa-caret-right"></i>  Kỹ năng điều hành doanh nghiệp</li>
+							<li><i class="fa fa-caret-right"></i>  Kỹ năng đàm phán kinh doanh</li>
+							<li><i class="fa fa-caret-right"></i>  Kỹ năng lập kế hoạch kinh doanh</li>
+							<li><i class="fa fa-caret-right"></i>  Phong thủy trong doanh nghiệp...</li>
+
+						</ul>
+						<p class="info"><strong>Ưu tiên</strong> những chuyên gia – giảng viên nổi tiếng, có uy tín trong ngành. Có kinh nghiệm thực tiễn và có kinh nghiệm giảng dạy ở lĩnh vực mà mình muốn giảng dạy.</p>
+					</div>
+					<p class="photo"><img src="./images/bg_about.png" alt="course 01" /></p>
+				</div>
+			</div>
+			
+		</div>
+		<div class="box_featured">
+			<div class="wrap">
+				<div class="box_middle">
+					<div class="box_item">
+						<p class="number">03</p>
+						<h3><span> Hợp tác giảng dạy với chúng tôi để:</span></h3>
+						<ul>
+							<li><i class="fa fa-caret-right"></i>  Quản trị Nhân sự</li>
+							<li><i class="fa fa-caret-right"></i>  Quản trị chiến lược</li>
+							<li><i class="fa fa-caret-right"></i>  Kỹ năng điều hành doanh nghiệp</li>
+							<li><i class="fa fa-caret-right"></i>  Kỹ năng đàm phán kinh doanh</li>
+							<li><i class="fa fa-caret-right"></i>  Kỹ năng lập kế hoạch kinh doanh</li>
+							<li><i class="fa fa-caret-right"></i>  Phong thủy trong doanh nghiệp...</li>
+
+						</ul>
+					</div>
+					<div class="box_item item_right">
+						<p class="number">04</p>
+						<h3><span> Quy trình hợp tác gồm 5 bước:</span></h3>
+						<ul>
+							<li><i class="fa fa-caret-right"></i>  Quản trị Nhân sự</li>
+							<li><i class="fa fa-caret-right"></i>  Quản trị chiến lược</li>
+							<li><i class="fa fa-caret-right"></i>  Kỹ năng điều hành doanh nghiệp</li>
+							<li><i class="fa fa-caret-right"></i>  Kỹ năng đàm phán kinh doanh</li>
+							<li><i class="fa fa-caret-right"></i>  Kỹ năng lập kế hoạch kinh doanh</li>
+							<li><i class="fa fa-caret-right"></i>  Phong thủy trong doanh nghiệp...</li>
+
+						</ul>
+					</div>
+
+				</div>
+			</div>
+		</div>
+		<!-- <div class="box_bottom">
+			<div class="wrap">
+				<h3 class="sst">GIÁ TRỊ CỐT LÕI</h3>
+				<div class="number">5</div>
+				<ul>
+					<li>
+						<div class="box_item">
+							<span>Tâm Huyết</span>
+							<div class="info"> SPO luôn nỗ lực không ngừng nhằm mang đến những giá trị tri thức tối ưu nhất cho cộng đồng doanh nhân Việt.</div>
+						</div>
+					</li>
+					<li>
+						<div class="box_item">
+							<span>Tâm Huyết</span>
+							<div class="info"> Đội ngũ giảng viên có trình độ chuyên môn cao, phương pháp phân tích giảng dạy phù hợp với người Việt.</div>
+						</div>
+					</li>
+					<li>
+						<div class="box_item">
+							<span>Tâm Huyết</span>
+							<div class="info"> SPO luôn chủ động cập nhật những giải pháp và phương thức quản trị, điều hành doanh nghiệp mới nhất ở trong và ngoài nước, giúp học viên tiếp cận được thêm nhiều tri thức mới, có giá trị thực tiễn cao.</div>
+						</div>
+					</li>
+
+				</ul>
+			</div>
+		</div> -->
+	</div>
+
+</div><!-- /#contents -->
+<?php include "./templates/footer.php"; ?>
+</body>
+</html>
