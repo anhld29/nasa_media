9122229c9b3ed2e478c6ac90abfd70c96d9752bc
huyenbt <huyenbt2208@gmail.com>
2018-11-01 16:44:05 +0700

    create about page

---
+++ about.php
@@ -0,0 +1,102 @@
+<?php $page = 'about'; include "./config/include.php"; ?>
+
+<!-- *** stylesheet *** -->
+<?php include "./templates/common_css.php"; ?>
+
+<!-- *** javascript *** -->
+<?php include "./templates/common_js.php"; ?>
+</head>
+
+<body id="<?php echo $page; ?>">
+<?php include "./templates/header.php"; ?>
+<div id="contents">
+	<div class="page-title">
+		<div class="wrap">
+			<h2>NASA Media</h2>
+			<div class="sub-title">Nền tảng khóa học trực tuyến về kỹ năng cho lãnh đạo và quản lý trung cấp</div>
+		</div>
+	</div>
+	<div class="con_about">
+		<div class="box_about">
+			<div class="wrap">
+				<div class="box_item">
+
+					<div class="box_info">
+						<p class="number">01</p>
+						<h3><span>Chúng tôi là ai?</span></h3>
+						<p class="info">
+						Tổ hợp Giáo dục và Đào tạo SPO/Nasa Media được hình thành bởi đội ngũ chuyên gia hàng đầu ở Việt Nam và thế giới trong lĩnh vực đào tạo, tư vấn cho doanh nghiệp. SPO là nền tảng khóa học trực tuyến về kỹ năng cho lãnh đạo và quản lý cấp trung với các nhóm
+						khóa học chuyên sâu dành riêng cho cộng đồng doanh nhân Việt: Lãnh đạo - quản lý, Nhân sự, Tài chính – kế toán, Sản xuất – vận hành, Tiếp thị - bán hàng, Kỹ năng chuyên nghiệp…</p>
+						<p class="author"> SPO/Nasa Media </p>
+					</div>
+					<p class="photo"><img src="./images/bg_about.png" alt="course 01" /></p>
+				</div>
+				<div class="box_item float_right">
+
+					<div class="box_info">
+						<p class="number">02</p>
+						<h3><span>Phương pháp đào tạo tại SPO?</span></h3>
+						<ul>
+							<li><i class="fa fa-caret-right"></i>  Học online: Mỗi một khóa học của SPO được giảng viên hệ thống thành nhiều bài học nhỏ, mỗi bài lại đi sâu vào nghiên cứu và phân tích một chuyên mục hoặc một vấn đề cụ thể. Vì vậy, học viên sẽ dễ dàng nắm bắt nội dung của từng phần cũng như theo dõi và xem lại tiến trình học.</li>
+							<li><i class="fa fa-caret-right"></i>  Học không giới hạn: Bạn có thể học ở bất cứ nơi đâu, bất cứ khi nào miễn là điện thoại, laptop, PC hoặc TV của bạn có kết nối mạng.</li>
+							<li><i class="fa fa-caret-right"></i>  Học trọn đời: Bạn chỉ cần mua 01 lần duy nhất và sẽ được sở hữu khóa học vĩnh viễn, ngay cả khi khóa học có những cập nhật, cải tiến sau này.</li>
+
+						</ul>
+					</div>
+					<p class="photo"><img src="./images/bg_about.png" alt="course 01" /></p>
+				</div>
+			</div>
+			
+		</div>
+		<div class="box_featured">
+			<div class="wrap">
+				<div class="box_middle">
+					<div class="box_item">
+						<p class="number">03</p>
+						<h3><span> TẦM NHÌN</span></h3>
+						<p class="info">
+						E-Learning (Giáo dục trực tuyến) là giải pháp giáo dục công nghệ cao, là xu hướng chung của giáo dục thế giới. Nó không chỉ là một lĩnh vực đầu tư tiềm năng mà còn là một sản phẩm nhân văn, mang lại cơ hội tiếp cập tri thức cho tất cả mọi người. Việc triển khai E-Learning trong giáo dục đào tạo là một xu hướng tất yếu nhằm đưa giáo dục Việt Nam tiếp cận với giáo dục thế giới.</p>
+					</div>
+					<div class="box_item item_right">
+						<p class="number">04</p>
+						<h3><span> SỬ MỆNH</span></h3>
+						<p class="info">
+						Trở thành một Hệ Sinh Thái Giáo Dục Trực Tuyến hàng đầu tại Việt Nam, là không gian mở kết nối trí tuệ, tầm nhìn và sự sáng tạo của đội ngũ Chuyên gia – Doanh nhân – Tri thức ở trong và ngoài nước.</p>
+					</div>
+
+				</div>
+			</div>
+		</div>
+		<div class="box_bottom">
+			<div class="wrap">
+				<h3 class="sst">GIÁ TRỊ CỐT LÕI</h3>
+				<div class="number">5</div>
+				<ul>
+					<li>
+						<div class="box_item">
+							<span>Tâm Huyết</span>
+							<div class="info"> SPO luôn nỗ lực không ngừng nhằm mang đến những giá trị tri thức tối ưu nhất cho cộng đồng doanh nhân Việt.</div>
+						</div>
+					</li>
+					<li>
+						<div class="box_item">
+							<span>Tâm Huyết</span>
+							<div class="info"> Đội ngũ giảng viên có trình độ chuyên môn cao, phương pháp phân tích giảng dạy phù hợp với người Việt.</div>
+						</div>
+					</li>
+					<li>
+						<div class="box_item">
+							<span>Tâm Huyết</span>
+							<div class="info"> SPO luôn chủ động cập nhật những giải pháp và phương thức quản trị, điều hành doanh nghiệp mới nhất ở trong và ngoài nước, giúp học viên tiếp cận được thêm nhiều tri thức mới, có giá trị thực tiễn cao.</div>
+						</div>
+					</li>
+
+				</ul>
+			</div>
+		</div>
+	</div>
+
+</div><!-- /#contents -->
+<?php include "./templates/footer.php"; ?>
+</body>
+</html>
